182c436d0a72655886bbbc1d6a53cb0dc605fa03
njpor <nnppkk.j@gmail.com>
2022-09-16 20:19:34 +0700

    Update connection_connect.php

--- connection_connect.php
+++ connection_connect.php
@@ -1,1 +1,17 @@
+<?php
+/*
+$servername = "localhost";
+$username = "db21_015";
+$password = "db21_015";
+$dbname = "db21_015";
 
+*/
+$conn = new mysqli($servername, $username, $password);
+mysqli_set_charset($conn, "utf8");
+if ($conn->connect_error) {
+    die("Connection failed: " .$conn->connect_error);
+}
+if (!$conn->select_db($dbname)) {
+    die("Connection failed: " .$conn->connect_error);
+}
+?>
