bd2e273460b83bca8bf886b412c7b5c2a43352d3
njpor <nnppkk.j@gmail.com>
2022-09-16 20:19:37 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,1 +1,28 @@
+<?php
+$controllers = array('homepage'=>['home', 'error']); //list controller
 
+
+function call($controller, $action){
+    require_once("controllers/".$controller."_controller.php");
+    switch($controller)
+    {
+       /* case "homepage":  $controller = new xxx_Controller();
+                      break;
+      */
+           
+                      
+    }
+
+    $controller->{$action}();
+}
+
+if(array_key_exists($controller, $controllers)) 
+{    if(in_array($action, $controllers[$controller]))
+    {    call($controller, $action); }
+    else
+    {    call('homepage', 'error'); }
+
+}
+else
+{    call('homepage', 'error');} 
+?>
