2b9fc415cab1c1cd308e5695c1d77ab3e2b99cc7
njpor <nnppkk.j@gmail.com>
2022-09-20 04:46:00 +0700

    Update shop.php

--- views/details/shop.php
+++ views/details/shop.php
@@ -1,1 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop KeyID</title>
 
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/designshop.css">
+
+</head>
+<body>
+    
+<!-- header section starts      -->
+
+<header>
+
+    <a href="#" class="logo"><i class="fas fa-gamepad"></i> keyId Shop</a>
+
+    <nav class="navbar">
+        <a class="active" href="#home">Home</a>
+        <a href="#menu">Valorant</a>
+        <a href="#">Genshin Impact</a>
+        <a href="#">Tower of Fantasy</a>
+    </nav>
+
+    <div class="icons">
+        <i class="fas fa-bars" id="menu-bars"></i>
+        <i class="fas fa-search" id="search-icon"></i>
+        <a href="#" class="fas fa-heart"></a>
+        <a href="#" class="fas fa-shopping-cart"></a>
+        <i class="fas fa-user"></i>
+    </div>
+
+</header>
+
+<!-- header section ends-->
+
+<!-- search form  -->
+
+<form action="" id="search-form">
+    <input type="search" placeholder="search here..." name="" id="search-box">
+    <label for="search-box" class="fas fa-search"></label>
+    <i class="fas fa-times" id="close"></i>
+</form>
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="swiper-container home-slider">
+
+        <div class="swiper-wrapper wrapper">
+
+            <div class="swiper-slide slide">
+                <div class="content">
+                    <span>Promotion</span>
+                    <h3>Valorant</h3>
+                    <p>detail...</p>
+                    <a href="#" class="btn">order now</a>
+                </div>
+                <div class="image">
+                    <img src="pic/home-img-1.png" alt="">
+                </div>
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="content">
+                    <span>Promotion</span>
+                    <h3>Genshin Impact</h3>
+                    <p>detail...</p>
+                    <a href="#" class="btn">order now</a>
+                </div>
+                <div class="image">
+                    <img src="pic/home-img-2.png" alt="">
+                </div>
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="content">
+                    <span>Promotion</span>
+                    <h3>Tower of Fantasy</h3>
+                    <p>detail...</p>
+                    <a href="#" class="btn">order now</a>
+                </div>
+                <div class="image">
+                    <img src="pic/home-img-3.png" alt="">
+                </div>
+            </div>
+
+        </div>
+
+        <div class="swiper-pagination"></div>
+
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+
+<!-- menu section starts  -->
+
+<section class="menu" id="menu">
+
+    <h3 class="sub-heading"> our menu </h3>
+    <h1 class="heading"> Valorant Thailand </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                </a><img src="pic/menu.jpg" alt="">
+            </div>
+            <div class="content">
+              <div class="stars">
+                <a href="#" ><i class="fas fa-exclamation-circle"></i></a>
+              </div>
+                <h3>Rank : IRON</h3>
+                <p>Full Reaver 2 Set - Odin,Vandal,Phanthom,...</p>
+                <a href="#" class="btn">add to cart</a>
+                <span class="price">฿2200</span>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="pic/menu.jpg" alt="">
+            </div>
+            <div class="content">
+              <div class="stars">
+                <a href="#" ><i class="fas fa-exclamation-circle"></i></a>
+              </div>
+                </a><h3>Rank : BRONZE</h3>
+                <p>Vandal : Nepetune, Phanthom : Ion, Sheriff : Magepunk </p>
+                <a href="#" class="btn">add to cart</a>
+                <span class="price">฿1200</span>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="pic/menu.jpg" alt="">
+            </div>
+            <div class="content">
+              <div class="stars">
+                <a href="#" ><i class="fas fa-exclamation-circle"></i></a>
+              </div>
+                <h3>Rank : SIVER</h3>
+                <p>Vandal : Sentinel, Operator : Reaver,Melee : Recon butterfly</p>
+                <a href="#" class="btn">add to cart</a>
+                <span class="price">฿1150</span>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="pic/menu.jpg" alt="">
+            </div>
+            <div class="content">
+              <div class="stars">
+                <a href="#" ><i class="fas fa-exclamation-circle"></i></a>
+              </div>
+                <h3>Rank : GOLD</h3>
+                <p>Full Reaver 2 Set - Odin,Vandal,Phanthom,...</p>
+                <a href="#" class="btn">add to cart</a>
+                <span class="price">฿999</span>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="pic/menu.jpg" alt="">
+                <a href="#" class="fas fa-heart"></a>
+            </div>
+            <div class="content">
+                <a href="#" ><h3>Rank : Diamond</h3></a>
+                <p>detail...</p>
+                <a href="#" class="btn">add to cart</a>
+                <span class="price">฿1800</span>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="pic/menu.jpg" alt="">
+                <a href="#" class="fas fa-heart"></a>
+            </div>
+            <div class="content">
+                <a href="#" ><h3>Rank : PLATINUM</h3></a>
+                <p>detail...</p>
+                <a href="#" class="btn">add to cart</a>
+                <span class="price">฿3150</span>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="pic/menu.jpg" alt="">
+                <a href="#" class="fas fa-heart"></a>
+            </div>
+            <div class="content">
+                <a href="#" ><h3>Rank : IMMORTAL</h3></a>
+                <p>detail...</p>
+                <a href="#" class="btn">add to cart</a>
+                <span class="price">฿2150</span>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="pic/menu.jpg" alt="">
+                <a href="#" class="fas fa-heart"></a>
+            </div>
+            <div class="content">
+                <a href="#" ><h3>Rank : RADIANT</h3></a>
+                <p>detail...</p>
+                <a href="#" class="btn">add to cart</a>
+                <span class="price">฿4050</span>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- menu section ends -->
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+    <div class="credit"> copyright @ 2022 by <span>Sleep on the bed.</span> </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/shopde.js"></script>
+
+</body>
+</html>
