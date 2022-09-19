192cb10e9286d4171d0ab7929831f82bcd2e9de4
njpor <nnppkk.j@gmail.com>
2022-09-20 04:46:27 +0700

    Create details.php

---
+++ views/details/details.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Details id</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/designshop.css">
+
+</head>
+<body>
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+    <h3 class="sub-heading"> about us </h3>
+    <h1 class="heading"> ID Valorant Game </h1>
+
+    <div class="row">
+
+        <div class="image">
+            <img src="pic/000.png" alt="">
+        </div>
+
+        <div class="content">
+            <h3>UID : #NNPPKK555</h3>
+            <h1>Description</h1>
+            <p>Rank : Diamond 
+                <br>Battle Pass : BTP 3 10 (Max) Characters: Complete [with new characters] 
+                <br>Name : Ready to change
+                <br>Price : 1800 Baht
+            </p>
+            
+            <a href="#" class="btn">add to cart</a>
+            <a href="#" class="btn">Buy Now</a>
+           
+        </div>
+
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+
+
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/shopde.js"></script>
+
+</body>
+</html>
