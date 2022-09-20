8bfe542ac950fdeb41aa17127195c553808652f3
Chavanlak <chavanlak1806@gmail.com>
2022-09-20 23:27:32 +0700

    Update register.php

--- views/member/register.php
+++ views/member/register.php
@@ -1,1 +1,47 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class= "header">
+        <h2>Welcom To KeyIdShop</h2>
+    </div>
+    
+    <from action= "Regiter"> 
+        <div class="input-group">
+            <label for="email address">Email</label>
+            <input type="email " name="email address">
+        </div>
+
+        <div class="input-group">
+            <label for="Phone number">Number</label>
+            <input type="number" name="Phone number">
+        </div>
+
+        <div class="input-group">
+            <label for="password_1">Password</label>
+            <input type="password" name="password_1">
+        </div>
+
+        <div class="input-group">
+            <label for="password_2">Comfirm Password</label>
+            <input type="password" name="password_2">
+        </div>
+
+        
+
+        <div class="input-group">
+            <button type="" name="Regiter" class= "btn">Register</button>
+        </div>
+        <p>By signing up, you are agreeing to our Terms & Conditions and Privacy Policy.<a href= "login.php">Login</a></p>
+    </from>
+    
+</body>
+</html>
+
