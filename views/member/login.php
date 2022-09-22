afc1cf22b4e8125e3c9f4318914ea6262ce707db
Chavanlak <chavanlak1806@gmail.com>
2022-09-22 20:14:45 +0700

    update login page

--- views/member/login.php
+++ views/member/login.php
@@ -1,39 +1,90 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class= "header">
-        <h2>Login</h2>
-    </div>
-    
-    <from action= "login"> 
-        <div class="input-group">
-            <label for="email">Email</label>
-            <input type="email" name="email">
-        </div>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Font Awesome -->
+        <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        rel="stylesheet"
+        />
+        <!-- Google Fonts -->
+        <link
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        rel="stylesheet"
+        />
+        <!-- MDB -->
+        <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.css"
+        rel="stylesheet"
+        />
+        <link rel="stylesheet" href="./login.css">
+        <title>Login</title>
+    </head>
+    <body>
+        <section class="vh-100">
+        <div class="container-fluid">
+            <center>
+                <div class="row">
 
-        <div class="input-group">
-            <label for="password_1">Password</label>
-            <input type="password" name="password_1">
-        </div>
+                    <div class="col-sm-8 text-black" >
 
-        <div class="input-group">
-            <label for="password_2">Comfirm Password</label>
-            <input type="password" name="password_2">
-        </div>
+                        <!-- <div class="px-5 ms-xl-4">
+                        <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
+                        <span class="h1 fw-bold mb-0">Logo</span>
+                        </div> -->
+                        <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5 div-from">
+                            <div class="card">
+                                <form class="card-body form-login" style="width: 23rem;" action="getuser.php" method="POST">
 
-        <div class="input-group">
-            <button type="Login" name="login_user" class= "btn">Login</button>
+                                <h1 class="fw-normal" style="letter-spacing: 1px;">Log in</h1>
+                                <p class="small mb-2 pb-lg-2 text-muted" style="font-size: 10px;">Enter your cerdential to access your account</p>
+
+                                <div class="align-items-center  mb-3" >
+                                    <button class="btn btn-light">
+                                        <img src="../../images/googleicon.png" alt=""  width="20" height="20">&nbsp; Login with google account
+                                    </button>
+                                    <p>or</p>
+                                </div>
+
+
+                                <div class="form-outline mb-4">
+                                    <input type="email" id="emailfiled" class="form-control form-control-lg" name="email" required/>
+                                    <label class="form-label" for="emailfiled">Email address</label>
+                                </div>
+
+                                <div class="form-outline mb-4">
+                                    <input type="password" id="passfield" class="form-control form-control-lg" name="pass" required/>
+                                    <label class="form-label" for="passfield">Password</label>
+                                </div>
+
+                                <div class="pt-1 mb-4">
+                                    <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+                                </div>
+
+                                <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">Forgot password?</a></p>
+                                <p>Don't have an account? <a href="register.php" class="link-info">Register here</a></p>
+
+                                </form>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="col-sm-4 px-0 d-none d-sm-block">
+                        <!-- <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img3.webp"
+                        alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;"> -->
+                        <img src="../../images/login.jpg" class="w-100 vh-100" style="object-fit: cover;object-position: left;"/>
+                    </div>
+                </div>
+
+            </center>
         </div>
-        <p>Don't have an accout?<a href= "register.php">Register here</a></p>
-    </from>
-    
-</body>
+        </section>
+        
+    </body>
+    <!-- MDB -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.js"></script>
 </html>
 
--- views/member/register.php
+++ views/member/register.php
@@ -1,47 +1,86 @@
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class= "header">
-        <h2>Welcom To KeyIdShop</h2>
-    </div>
-    
-    <from action= "Regiter"> 
-        <div class="input-group">
-            <label for="email address">Email</label>
-            <input type="email " name="email address">
-        </div>
+    <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <!-- Font Awesome -->
+            <link
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+            rel="stylesheet"
+            />
+            <!-- Google Fonts -->
+            <link
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            rel="stylesheet"
+            />
+            <!-- MDB -->
+            <link
+            href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.css"
+            rel="stylesheet"
+            />
+            <link rel="stylesheet" href="./login.css">
+            <title>Register</title>
+    </head>
+    <body>
+        <section class="vh-100">
+            <div class="container-fluid">
+                <center>
+                    <div class="row">
 
-        <div class="input-group">
-            <label for="Phone number">Number</label>
-            <input type="number" name="Phone number">
-        </div>
+                        <div class="col-sm-8 text-black" >
 
-        <div class="input-group">
-            <label for="password_1">Password</label>
-            <input type="password" name="password_1">
-        </div>
+                            <!-- <div class="px-5 ms-xl-4">
+                            <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
+                            <span class="h1 fw-bold mb-0">Logo</span>
+                            </div> -->
+                            <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5 div-from">
+                                <div class="card">
+                                    <form class="card-body form-login" style="width: 23rem;" action="getregister.php" method="POST">
+                                        <h4 class="fw-normal" style="letter-spacing: 1px;">Welcome To KeyIdShop</h4>
+                                        <p class="small mb-2 pb-lg-2 text-muted" style="font-size: 10px;">Already account&nbsp; <a href="login.php">Login</a></p>
 
-        <div class="input-group">
-            <label for="password_2">Comfirm Password</label>
-            <input type="password" name="password_2">
-        </div>
+                                        <div class="form-outline mb-4">
+                                            <input type="email" id="emailfiled" class="form-control form-control-lg" name="email" required />
+                                            <label class="form-label" for="emailfiled">Email address</label>
+                                        </div>
 
-        
+                                        <div class="form-outline mb-4">
+                                            <input type="tel" id="typePhone" class="form-control form-control-lg" name="phone" />
+                                            <label class="form-label" for="typePhone">Phone number input</label>
+                                        </div>
 
-        <div class="input-group">
-            <button type="" name="Regiter" class= "btn">Register</button>
-        </div>
-        <p>By signing up, you are agreeing to our Terms & Conditions and Privacy Policy.<a href= "login.php">Login</a></p>
-    </from>
-    
-</body>
+                                        <div class="form-outline mb-4">
+                                            <input type="password" id="passfield" class="form-control form-control-lg" name="pass" required />
+                                            <label class="form-label" for="passfield">Password</label>
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <input type="password" id="confirmpassfield" class="form-control form-control-lg" name="confirmpass" required />
+                                            <label class="form-label" for="confirmpassfield">Confirm passwords</label>
+                                        </div>
+
+                                        <div class="pt-1 mb-4">
+                                            <button class="btn btn-primary btn-lg btn-block" type="submit">Register</button>
+                                        </div>
+                                        
+                                        <p style="font-size: 10px;">By signing up, you are agreeing to our <a href= "#">Terms & Conditions</a> and <a href= "#">Privacy Policy</a>.</p>
+                                    </form>
+                                </div>
+
+                            </div>
+
+                        </div>
+                        <div class="col-sm-4 px-0 d-none d-sm-block">
+                            <!-- <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img3.webp"
+                            alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;"> -->
+                            <img src="../../images/register.jpg" class="w-100 vh-100" style="object-fit: cover;object-position: left;"/>
+                        </div>
+                    </div>
+
+                </center>
+            </div>
+        </section>
+    </body>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.js"></script>
 </html>
-
